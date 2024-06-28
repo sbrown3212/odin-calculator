f9b173641667d6e99d42cb47981a41ccdf38e56d
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2024-06-28 11:37:00 -0600

    Add basic operator functions

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+// debugging purposes only
+let a = 2;
+let b = 3;
