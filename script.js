d4906ffb1704fbeb5b1d484c644b5f7d61da88fe
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2024-06-28 14:29:07 -0600

    Add operate() function

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-
+// Operator functions
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+// User input
+let n1;
+let op;
+let n2;
+
 // debugging purposes only
-let a = 2;
-let b = 3;
+n1 = 2;
+n2 = 3;
+
+// Operator characters
+const addOp = "+";
+const subtractOp = "-";
+const multiplyOp = "x";
+const divideOp = "/";
+
+const operate = (operator, a, b) => {
+    if (operator === addOp) {
+        return add(a, b);
+    } else if (operator === subtractOp) {
+        return subtract(a, b);
+    } else if (operator === multiplyOp) {
+        return multiply(a, b);
+    } else if (operator === divideOp) {
+        return divide(a, b);
+    }
+}
