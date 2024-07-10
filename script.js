248352995c740590ff8d76dbf916289aa4df1a2f
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2024-07-10 11:19:14 -0600

    Prevent divide by zero and multiple decimal points
    When user tries to divide by zero, an alert displays and all values are
    set to null. Also, if a number already contains a decimal point and the
    user clicks the decimal button again, nothing will happen.

--- script.js
+++ script.js
@@ -74,33 +74,42 @@ buttons.addEventListener("click", (event) => {
 
     // if n1 is equal to number (or string) allow operators except for "equals"
     if (operator === null) {
-      if (event.target.classList.contains("num") && isResult === false) {
-        // if target is number, concat n1 with event.target.value
-        display.textContent += event.target.value;
-        n1 = display.textContent;
-        return;
-      } else if (event.target.classList.contains("num") && isResult === true) {
+      if (event.target.classList.contains("num") && isResult === true) {
+        // if display is a result and user clicks new number, delete result and save new number as n1 (to avoid concatenating numbers to a result from previous operation)
         display.textContent = event.target.value;
         n1 = display.textContent;
         isResult = false;
         return;
+      } else if (event.target.value === "." && isResult === false && n1.includes(".")) {
+        // do nothing if n1 already has a decimal
+        return;
+      } else if (event.target.classList.contains("num") && isResult === false) {
+        // if target is number, concat n1 with button value
+        display.textContent += event.target.value;
+        n1 = display.textContent;
+        return;
       } else if (event.target.classList.contains("arithmetic")) {
-        // if target is operator, assign operator to event.target.value
+        // if target is operator, assign operator to button value
         display.textContent += " " + event.target.value + " ";
         operator = event.target.value;
         return;
       }
     }
 
-    // if operator is not equal to null, have next numbers add values to n2
+    // if n1 and operator have been assigned
     if (n2 === null) {
       if (event.target.classList.contains("num")) {
+        // if n2 has no value assigned yet
         display.textContent += event.target.value;
         n2 = event.target.value;
         return;
       }
     } else {
-      if (event.target.classList.contains("num")) {
+      if (event.target.value === "." && n2.includes(".")) {
+        // prevent more than one decimal in n2
+        return;
+      } else if (event.target.classList.contains("num")) {
+        // concatenate button value to end of n2
         display.textContent += event.target.value;
         array = display.textContent.split(" ");
         n2 = array[array.length-1];
@@ -108,12 +117,21 @@ buttons.addEventListener("click", (event) => {
       }
     }
 
-    // if arithmetic operator is clicked when n1, n2, and operator != null
+    // if arithmetic operator is clicked when n1, n2, and operator have been assigned
     if (n1 && n2 && operator && event.target.classList.contains("arithmetic")) {
+      if (operator === divideOp && n2 === "0") {
+        // prevent divide by zero
+        alert("Can't divide by zero!");
+        n1 = n2 = operator = null;
+        display.textContent = "0";
+        return;
+      }
+      // operate, update display, and save result to n1
       let result = operate(operator, n1, n2);
       display.textContent = result;
       n1 = result;
       
+      // assign operator based on value of button clicked
       display.textContent += " " + event.target.value + " ";
       operator = event.target.value;
       
@@ -125,7 +143,14 @@ buttons.addEventListener("click", (event) => {
 })
 
 equals.addEventListener("click", () => {
-  // update display with result for operate function
+  if (operator === divideOp && n2 === "0") {
+    // prevent divide by zero
+    alert("Can't divide by zero!");
+    n1 = n2 = operator = null;
+    display.textContent = "0";
+    return;
+  }
+  // operate, update display, and save result to n1
   let result = operate(operator, n1, n2);
   display.textContent = result;
   n1 = result;
@@ -139,33 +164,35 @@ equals.addEventListener("click", () => {
 edit.addEventListener("click", (event) => {
   if (event.target.classList.contains("btn")) {
     if (event.target.value === "clear") {
+      // clear display and assign all values to null
       display.textContent = "0";
       n1 = n2 = operator = null;
       return;
     } else if (event.target.value === "delete") {
       // delete next character from display
       if (display.textContent.slice(-1) === " ") {
-        // when next character to be deleted is a space, delete the next 3 characters (ex. space, operator, space)
+        // delete operator and spaces on either side
         display.textContent = display.textContent.slice(0, -3);
         operator = null;
       } else {
+        // delete last character
         display.textContent = display.textContent.slice(0, -1);
       }
 
       let array = display.textContent.split(" ");
 
       if (array[0] === "") {
-        // when the last digit of n1 has been deleted
+        // set n1 to null and set display to "0" when last digit of n1 is deleted
         n1 = null;
         display.textContent = "0";
       } else if (array[2] === "") {
-        // when the last digit of n2 has been deleted
+        // set n2 to null when last digit of n2 is deleted
         n2 = null;
       } else if (array.length === 1) {
-        // when a digit from n1 has been deleted
+        // update n1
         n1 = array[array.length - 1];
       } else if (array.length === 3) {
-        // when a digit from n2 has been deleted
+        // update n2
         n2 = array[array.length - 1];
       }
 
@@ -174,6 +201,4 @@ edit.addEventListener("click", (event) => {
   }
 })
 
-// TODO: display snarky message if dividing by zero
-// TODO: EXTRA CREDIT prevent more than one decimal in a number
 // TODO: EXTRA CREDIT add keyboard support
